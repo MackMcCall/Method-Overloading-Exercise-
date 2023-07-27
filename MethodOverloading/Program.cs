1c0b9690ef62623df89ffc2a15a15c0bd250f330
Mack McCall <mackmc9924@gmail.com>
2023-07-27 10:41:04 -0600

    I think this is right, instructions sl. vague

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -6,5 +6,26 @@
         {
             Console.WriteLine("Hello, World!");
         }
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+        public static string Add(int num1, int num2, bool isDollar)
+        {
+            int sum = num1 + num2;
+            if (isDollar == true)
+            {
+                if (sum > 1)
+                {
+                    return $"${sum} dollars";
+                }
+                return $"${sum} dollar";
+            }
+            return $"{sum}";
+        }
     }
 }
